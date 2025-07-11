0b658bef56b1748c0ed0629756926d778a4d81e6
Gabriel Bran <bra23590@uvg.edu.gt>
2025-07-10 20:34:54 -0600

    Polygon 4

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -24,7 +24,7 @@ fn main() {
     //line(&mut frame_buffer, Vector2::new(100.0, 100.0), Vector2::new(200.0, 200.0));
 
     //Poligono con array con polygon.rs
-    let polygon1 = vec![
+    /*let polygon1 = vec![
         (165, 380), (185, 360), (180, 330), (207, 345), (233, 330), (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)
     ];
 
@@ -64,9 +64,36 @@ fn main() {
         frame_buffer.set_current_color(Color::RED);
         fill_polygon(&mut frame_buffer, &polygon3_points);
         frame_buffer.set_current_color(Color::WHITE);
-        draw_polygon(&mut frame_buffer, &polygon3_points);
+        draw_polygon(&mut frame_buffer, &polygon3_points);*/
     
+    let polygon4 = vec![
+        (413, 177), (448, 159), (502, 88), (553, 53), (535, 36), (676, 37), (660, 52), (750, 145), (761, 179), (672, 192), (659, 214), (615, 214), (632, 230), (580, 230), (597, 215), (552, 214), (517, 144), (466, 180)
+    ];
 
-    let output_file = "polygon3.bmp";
+    let polygon4_points: Vec<Vector2> = polygon4
+        .into_iter()
+        .map(|(x, y)| Vector2::new(x as f32, y as f32))
+        .collect();
+
+    frame_buffer.set_current_color(Color::GREEN);
+    fill_polygon(&mut frame_buffer, &polygon4_points);
+    frame_buffer.set_current_color(Color::WHITE);
+    draw_polygon(&mut frame_buffer, &polygon4_points);
+
+    let polygon5 = vec![
+        (682, 175), (708, 120), (735, 148), (739, 170)
+    ];
+
+    let polygon5_points: Vec<Vector2> = polygon5
+        .into_iter()
+        .map(|(x, y)| Vector2::new(x as f32, y as f32))
+        .collect();
+
+    frame_buffer.set_current_color(Color::PURPLE);
+    fill_polygon(&mut frame_buffer, &polygon5_points);
+    frame_buffer.set_current_color(Color::WHITE);
+    draw_polygon(&mut frame_buffer, &polygon5_points);
+
+    let output_file = "polygon4.bmp";
     frame_buffer.render_to_file(output_file);
 }
